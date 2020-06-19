b72e499700d4106d11e337750663b51062df4298
Luckner Cyné <luckner@cyne.fr>
2020-06-19 18:12:13 +0200

    make GameBoard factory and functions

--- src/factories.test.js
+++ src/factories.test.js
@@ -11,22 +11,111 @@ describe('A ship', () => {
     expect(factories.Ship('hi')).toHaveProperty('length', 1);
   });
 
-  test('has a gotHitAt property', () => {
-    expect(factories.Ship(2)).toHaveProperty('gotHitAt', [false, false]);
+  test('has a damagedAt property', () => {
+    expect(factories.Ship(2)).toHaveProperty('damagedAt', [false, false]);
   });
 
   test('has hit() method to mark a position as hit', () => {
     const testShip = factories.Ship(2);
-    testShip.hit(1);
-    expect(testShip).toHaveProperty('gotHitAt', [false, true]);
+    expect(testShip.hit(1)).toHaveProperty('damagedAt', [false, true]);
   });
 
   test('has isSunk() method', () => {
-    const shipToSink = factories.Ship(3);
+    let shipToSink = factories.Ship(3);
     expect(shipToSink.isSunk()).toBe(false);
-    shipToSink.hit(0);
-    shipToSink.hit(1);
-    shipToSink.hit(2);
+    shipToSink = shipToSink.hit(0);
+    shipToSink = shipToSink.hit(1);
+    shipToSink = shipToSink.hit(2);
     expect(shipToSink.isSunk()).toBe(true);
+  });
+});
+
+describe('A GameBoard', () => {
+  const boardBefore = factories.GameBoard(4);
+  beforeEach(() => {
+    boardBefore.reset();
+  });
+  const Cell = factories.Cell;
+  const testShip = factories.Ship(2);
+  const cell0 = { shipIndex: 0, hitIndex: 0, wasAttacked: false };
+  const cell1 = { shipIndex: 0, hitIndex: 1, wasAttacked: false };
+  const emptyBoard = [
+    [Cell(), Cell(), Cell(), Cell()],
+    [Cell(), Cell(), Cell(), Cell()],
+    [Cell(), Cell(), Cell(), Cell()],
+    [Cell(), Cell(), Cell(), Cell()],
+  ];
+  test('has cells that refer to ship positions', () => {
+    expect(boardBefore).toHaveProperty('cells', emptyBoard);
+  });
+
+  test('can addShip() vertically', () => {
+    const boardAfterVertical = [
+      [Cell(), Cell(), Cell(), Cell()],
+      [Cell(), Cell(), cell0, Cell()],
+      [Cell(), Cell(), cell1, Cell()],
+      [Cell(), Cell(), Cell(), Cell()],
+    ];
+    expect(boardBefore.addShip(testShip, 1, 2, true)).toHaveProperty(
+      'cells',
+      boardAfterVertical
+    );
+  });
+
+  test('can addShip() horizontally', () => {
+    const boardAfterHorizontal = [
+      [Cell(), Cell(), Cell(), Cell()],
+      [Cell(), Cell(), cell0, cell1],
+      [Cell(), Cell(), Cell(), Cell()],
+      [Cell(), Cell(), Cell(), Cell()],
+    ];
+    expect(boardBefore.addShip(testShip, 1, 2, false)).toHaveProperty(
+      'cells',
+      boardAfterHorizontal
+    );
+  });
+
+  test("doesn't add a ship if too low", () => {
+    expect(boardBefore.addShip(testShip, 3, 0, true)).toHaveProperty(
+      'cells',
+      emptyBoard
+    );
+  });
+
+  test("doesn't add a ship if too far right", () => {
+    expect(boardBefore.addShip(testShip, 0, 3, false)).toHaveProperty(
+      'cells',
+      emptyBoard
+    );
+  });
+
+  test("doesn't add a ship overlapping an existing one", () => {
+    const otherShip = factories.Ship(3);
+    const withOverlap = boardBefore
+      .addShip(testShip, 0, 0, true)
+      .addShip(otherShip, 1, 0, false);
+    // We expect to get the ship we placed first,
+    // not the 2nd one to overlap it
+    expect(withOverlap.ships[0]).not.toEqual(otherShip);
+  });
+
+  test("Doesn't add the same ship twice", () => {
+    const attemptedDuplicate = boardBefore
+      .addShip(testShip, 0, 0, true)
+      .addShip(testShip, 1, 1, true);
+    expect(attemptedDuplicate.ships[1]).toBeUndefined();
+  });
+
+  test('calls Ship.hit() if receiveAttack() on one', () => {
+    const attacked = boardBefore.addShip(testShip, 0, 0, false);
+    expect(attacked.receiveAttack(0, 0).ships[0]).toEqual(testShip.hit(0));
+  });
+
+  test('Tells if all the ships have sunk', () => {
+    const shipwreck = boardBefore
+      .addShip(testShip, 0, 0, true)
+      .receiveAttack(0, 0)
+      .receiveAttack(1, 0);
+    expect(shipwreck.isAllSunk()).toBe(true);
   });
 });
