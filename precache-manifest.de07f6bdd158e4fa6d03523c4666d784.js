7cabb926da80ae1cddb8f66d3b4b2ef2eea67b0e
Luckner Cyné <luckner@cyne.fr>
2020-07-24 21:03:42 +0200

    Updates

---
+++ precache-manifest.de07f6bdd158e4fa6d03523c4666d784.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2db9576806c1037314f94b4e609061aa",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "a1f0138aed7cf8fb7b11",
+    "url": "/battleship/static/css/main.63926e74.chunk.css"
+  },
+  {
+    "revision": "db2092cd59908ccc072a",
+    "url": "/battleship/static/js/2.74f4f068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/battleship/static/js/2.74f4f068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1f0138aed7cf8fb7b11",
+    "url": "/battleship/static/js/main.62a0cd21.chunk.js"
+  },
+  {
+    "revision": "1576b120f8ea920fbd40",
+    "url": "/battleship/static/js/runtime-main.9e463cc9.js"
+  }
+]);
